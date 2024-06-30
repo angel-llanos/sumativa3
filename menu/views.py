5c1bb0314932aad237da900e377ca457b9c23d01
rorro <rodguzm03@gmail.com>
2024-06-29 23:33:56 -0400

    creacion de inicio

--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from autos.models import Autos
 # Create your views here.
 
 def index(request):
-    context = {}
+    vehiculos = Autos.objects.all()
+    context = {
+        'vehiculos': vehiculos
+    }
     return render(request, "menu/index.html", context)
